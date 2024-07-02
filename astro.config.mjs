11a48345fcbda9d9291501e0eee800d611bb1162
Jesus Borrero <jesufer2018@hotmail.com>
2024-07-02 14:04:13 -0500

    change font and format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://resume.jesubohrdev.com",
   integrations: [tailwind()]
-});
+})
